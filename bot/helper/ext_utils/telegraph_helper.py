38df174a741ad600d4a66028599d1c54d8f0c2f4
Tahir <tahirnew@nori24.tv>
2023-09-07 13:14:17 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -76,6 +76,6 @@ class TelegraphHelper:
         return
 
 
-telegraph = TelegraphHelper('Aeon', 'https://t.me/ProjectAeon')
+telegraph = TelegraphHelper('𝙎𝙝𝙤𝙂𝙪𝙉', 'https://t.me/ShoGuNPacKs')
 
 bot_loop.run_until_complete(telegraph.create_account())
